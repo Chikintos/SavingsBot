ac054af6b70239a31c7fc72bead7a30bc9baa185
Ch1k1ta <93987118+Chikintos@users.noreply.github.com>
2023-08-16 03:07:23 +0300

    Update load_django.py
    fix bug with upload Django system

--- modules/bot/load_django.py
+++ modules/bot/load_django.py
@@ -1,7 +1,13 @@
 import sys
 import os
 import django
+from pathlib import Path
 
-sys.path.append('savingsBot')
+
+
+current_directory = Path(__file__).resolve().parent.parent.parent
+savingsBot_path = current_directory / 'savingsBot'
+savingsBot_path=savingsBot_path.as_posix().replace('/', '\\')
+sys.path.append(savingsBot_path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'savingsBot.settings'
 django.setup()
